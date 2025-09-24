884bb9b28e176bdb1807f0ca998c34a4b5eefcea
redeemed41 <144100116+redeemed41@users.noreply.github.com>
2025-09-24 08:05:10 +0800

    invoice setup

--- backend/invoice.js
+++ backend/invoice.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const Booking = require("./models/booking");
-const User = require("../models/user");
+const User = require("./models/user");
 
 module.exports = function (sendBookingNotification) {
   /**
