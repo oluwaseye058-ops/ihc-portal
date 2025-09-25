ec9f2762dabeeabb72703cb07ec8a197af86634c
redeemed41 <144100116+redeemed41@users.noreply.github.com>
2025-09-25 17:23:36 +0800

    final mouting

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.options("*", cors());
+app.options(/.*/, cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/auth")(sendBookingNotification);
